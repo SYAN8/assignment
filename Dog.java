fde000f1e69b5dd1ee9b011e086bfda47ae840af
SYAN8 <44732128+SYAN8@users.noreply.github.com>
2018-11-11 02:31:40 +0000

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,12 +1,12 @@
 class Dog {
+	//Indicates the horizontal axis of dog
 	public int x;
+	//Indicates the coordinate of dog
 	public int y;
 
 	public Dog(){
-		//表示乌龟的横坐标
+	//default value of cordinate is -1
 		x = -1;
-
-		//表示乌龟的纵坐标
 		y = -1;
 	}
 	public Dog(int x, int y){
@@ -16,11 +16,9 @@ class Dog {
 	public int getX() {
 		return x;	
 	}
-
 	public void setX(int x) {
 		this.x = x;
 	}
-
 	public void setY(int y) {
 		this.y = y;
 	}
